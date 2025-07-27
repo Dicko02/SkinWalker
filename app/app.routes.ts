b72ee9379a684aae0ccf58b9f1e7bd75b240f5ba
Dicko02 <dickfack123@gmail.com>
2025-07-27 14:22:54 +0000

    fixed

--- app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { AllSkinWalkerComponent } from './all-skin-walker/all-skin-walker.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-
-export const routes: Routes = [
-    {path: 'SkinWalkers', component: AllSkinWalkerComponent},
-    {path: '', component: LandingPageComponent}
-];
